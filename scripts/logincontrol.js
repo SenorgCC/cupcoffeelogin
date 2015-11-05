2221b5517f0631d62cbc3aa3a83a0751252e0f26
SenorgCC <Alex.Sinicyn@gmx.de>
2015-11-05 18:11:41 +0100

    login function simple eingefügt. erste idee für die registrierung. keine Datenbank

--- scripts/logincontrol.js
+++ scripts/logincontrol.js
@@ -4,12 +4,42 @@
 var app = angular.module('Cupcoffeelogin', [])
     .controller('logincontroller', function($scope){
 
-        $scope.submit=function(username, password){
-            alert(username);
+        $scope.submit=function(username, userpassword) {
+            //loginflag
+            var logintest = false;
+
+            //databasedummy
+            $scope.accounts = [
+                {id: 0, name: "test", passwort: "kek"},
+                {id: 1, name: "admin", passwort: "admin"},
+                {id: 2, name: "test2", passwort: "ke22k"}
+            ];
+
+            //suchenachkorrekten eingaben
+            for (var i = 0; i <= $scope.accounts.length; i++) {
+                if ($scope.accounts[i].name == username) {
+                    if ($scope.accounts[i].passwort == userpassword) {
+                        alert("WILLKOMMEN!");
+                        //Hierhinkommt die weiterleitung
+                        //keine weitere durchsuchung der schleife nötig, da einmalige nutzter
+                        break;
+                    }else{
+                        alert("FALSCHER INPUT!");
+                        //eingaben werden gecleart
+                        $scope.username='';
+                        $scope.userpassword='';
+                        break;
+                    }
+                }
+            }
         };
         $scope.register=function(){
-            $scope.ausgabe="nope!";
+            //öffnet ein neues fenster zum registrieren des nutzters
         };
     });
+app.controller('registercontroller',function($scope){
+
+
+});
 
 
